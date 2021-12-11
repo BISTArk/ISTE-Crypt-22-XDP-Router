c653a9f0c8b9a0872583f78158f2612dff635429
achyutshegade <64579922+achyutshegade@users.noreply.github.com>
2021-12-11 21:19:48 +0530

    Add files via upload

---
+++ Achyut/server-router-router-client.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sudo ip netns add server1
+sudo ip netns add client1
+
+sudo ip link add v-net-0 type bridge
+sudo ip link add v-net-1 type bridge
+sudo ip link set dev v-net-0 up
+sudo ip link set dev v-net-1 up
+sudo ip link add veth0 type veth peer name veth0-br
+sudo ip link add veth1 type veth peer name veth1-br
+sudo ip link add veth-br-0 type veth peer name veth-br-1
+
+sudo ip link set veth0 netns server1
+sudo ip link set veth0-br master v-net-0
+sudo ip link set veth-br-0 master v-net-0
+sudo ip link set veth-br-1 master v-net-1
+sudo ip link set veth1 netns client1
+sudo ip link set veth1-br master v-net-1
+
+sudo ip netns exec server1 ip addr add 192.168.15.1 dev veth0
+sudo ip netns exec client1 ip addr add 192.168.15.2 dev veth1
+
+sudo ip netns exec server1 ip link set dev veth0 up
+sudo ip netns exec client1 ip link set dev veth1 up
+
